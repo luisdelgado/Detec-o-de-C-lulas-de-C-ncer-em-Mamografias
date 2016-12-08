fb622966b30ea3f7ae2cb34c4ce8b135ea1170a7
Rayana Alencar <rvsa2@cin.ufpe.br>
2016-12-08 12:57:55 -0300

    Add files via upload

--- kmedias/kmeans.py
+++ kmedias/kmeans.py
@@ -1,14 +1,16 @@
 import numpy as np
 from sklearn.cluster import KMeans
 CLASS_INDEX = 6
-a = np.loadtxt('mammography-consolidatedSemDuplicada.csv', delimiter=',') 
+a = np.loadtxt('classe0.csv', delimiter=',') 
 class0 = np.array([row for row in a])
-for x in range(class0):
-	print(x)
+
 
 kmeans = KMeans(n_clusters=260).fit(class0)
 
-for x in range(kmeans):
-	print(x)
+final =[];
+for x in range(len(kmeans.cluster_centers_)):
+	final.append(kmeans.cluster_centers_[x])
 
-np.savetxt("kmeansDaClasse0.csv", a, delimiter=",",fmt='%.8f')
+print(len(final))
+
+np.savetxt("kmeansDaClasse0.csv", final, delimiter=",",fmt='%.8f')
